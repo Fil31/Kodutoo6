bc35a5b0d61f80bbaebbab582c27067e4b74dd79
Fil31 <101242957+Fil31@users.noreply.github.com>
2023-01-15 13:13:22 +0200

    Добавьте файлы проекта.

---
+++ Kodutoo6/Kodutoo6.py
@@ -0,0 +1,10 @@
+
+from module1 import *
+
+answer=arithmetic(2.5,"+",1.8)
+print(answer)
+
+XOR_cipher(int(input("Введите пароль")), int(input("Введите ключ шифрования")))
+
+XOR_uncipher(int(input("Введи ключ шифрования")), int(input("Введи ключ расшировки")))
+is_valid_date(15,2,1999)
